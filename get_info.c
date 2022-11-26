7c7ceaaede8f2aacb62816ea9b537a600d5b8b83
harold <harold10@uos.ac.kr>
2022-11-26 13:16:16 +0900

    First commit

---
+++ get_info.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <curl/curl.h>
+
+extern char URL[50];
+
+int main(void) {
+
+    CURL *curl = curl_easy_init();
+
+    if(curl) {
+        CURLcode res;
+        curl_easy_setopt(curl, CURLOPT_URL, URL);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+    }
+
+    else {
+        printf("[error] curl init failed\n");
+        return 1;
+    }
+
+    return 0;
+}
