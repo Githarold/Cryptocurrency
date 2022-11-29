bf354441820d75a63d0a6d1a406adbcc88d6229d
grwise <grwise0906@gmail.com>
2022-11-29 13:29:12 +0900

    get_info by using json-c

--- c_2022/project/BTC/BTC1/get_info.c
+++ c_2022/project/BTC/BTC1/get_info.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <json-c/json.h>
 
-char URL[BUFSIZE] = "https://api.bithumb.com/public/ticker/btc_KRW";
+char URL[BUFSIZE] = "https://api.bithumb.com/public/ticker/eth_KRW";
 
 int main(void) {
     FILE *fp;
@@ -29,6 +29,8 @@ int main(void) {
     res = curl_easy_perform(curl);
     curl_easy_cleanup(curl);
 
+    fclose(fp);
+
     //######################################################## 
     json_object *result = json_object_from_file("result.json");    
 
@@ -37,10 +39,12 @@ int main(void) {
     // char *buff = "{\"status\":\"0000\",\"data\":{\"opening_price\":\"22390000\",\"closing_price\":\"22349000\",\"min_price\":\"22346000\",\"max_price\":\"22414000\",\"units_traded\":\"53.72523138\",\"acc_trade_value\":\"1202107981.1507\",\"prev_closing_price\":\"22390000\",\"units_traded_24H\":\"4376.45251621\",\"acc_trade_value_24H\":\"98706032672.7421\",\"fluctate_24H\":\"-340000\",\"fluctate_rate_24H\":\"-1.5\",\"date\":\"1669648961894\"}}";
     // json_object *result = json_tokener_parse(buff);
 
-    json_object *data, *price;
+    json_object *data, *price, *fluctuate_rate;
     data = json_object_object_get(result, "data");
 
     price = json_object_object_get(data, "closing_price");
-    printf("price : %s\n", json_object_get_string(price));
+    fluctuate_rate = json_object_object_get(data, "fluctate_rate_24H");
+    printf("price : %s 원\n", json_object_get_string(price));
+    printf("변동률 : %s %%\n", json_object_get_string(fluctuate_rate));
     return 0;
 }
